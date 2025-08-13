74b39df5f47266d8bf298f83aa1ac43d6d55bf20
Emerson Jara <emerson.jara1998@gmail.com>
2025-08-13 18:33:00 -0500

    Initial Commit

---
+++ SistemaCitasMedicas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SistemaCitasMedicas
+//
+//  Created by Emerson Jara Gamarra on 13/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
